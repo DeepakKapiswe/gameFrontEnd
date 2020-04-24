cbdf9c9b881738a77836879c1c99294b18146259
deepakkapiswe <deepakkapiswe@gmail.com>
2020-04-24 15:33:45 +0530

    add game reset and show robo direction
    Signed-off-by: deepakkapiswe <deepakkapiswe@gmail.com>

--- src/App.js
+++ src/App.js
@@ -13,16 +13,21 @@ const turnUpUrl  = 'http://192.168.43.28:7000/turnUp';
 const turnRightUrl = 'http://192.168.43.28:7000/turnRight';
 const turnDownUrl  = 'http://192.168.43.28:7000/turnDown';
 const turnLeftUrl  = 'http://192.168.43.28:7000/turnLeft';
+const resetGameUrl     = 'http://192.168.43.28:7000/resetGame';
+
+
 
 function App() {
   const [url, setUrl] = useState(getGameUrl);
   const [count, setCount] = useState(0);
+  const [roboDir, setRoboDir] = useState("UP")
   
-  const move      = () => { setUrl(moveReqUrl);  setCount(count + 1); }
-  const turnUp    = () => { setUrl(turnUpUrl);   setCount(count + 1); }
-  const turnRight = () => { setUrl(turnRightUrl);setCount(count + 1); }
-  const turnDown  = () => { setUrl(turnDownUrl); setCount(count + 1); }
-  const turnLeft  = () => { setUrl(turnLeftUrl); setCount(count + 1); }
+  const move      = () => { setUrl(moveReqUrl);   setCount(count + 1); }
+  const turnUp    = () => { setUrl(turnUpUrl);    setRoboDir("UP"); setCount(count + 1); }
+  const turnRight = () => { setUrl(turnRightUrl); setRoboDir("RIGHT"); setCount(count + 1); }
+  const turnDown  = () => { setUrl(turnDownUrl);  setRoboDir("DOWN"); setCount(count + 1); }
+  const turnLeft  = () => { setUrl(turnLeftUrl);  setRoboDir("LEFT"); setCount(count + 1); }
+  const resetGame = () => { setUrl(resetGameUrl); setCount(count + 1); }
 
   // useEffect(() => {
   //   const interval = setInterval(() => {
@@ -31,6 +36,17 @@ function App() {
   //   }, 600);
   //   return () => clearInterval(interval);
   // }, []);
+
+  function RoboDir() {
+
+
+  if (roboDir === "UP") {return <h2>UP</h2>; }
+  if (roboDir === "RIGHT") {return <h2>RIGHT</h2>; }
+  if (roboDir === "DOWN") {return <h2>DOWN</h2>; }
+  if (roboDir === "LEFT") {return <h2>LEFT</h2>; }
+  
+  }
+  
 
 
   return (
@@ -112,6 +128,21 @@ function App() {
             </Button>
         </Grid>
       </Grid>
+      <Grid container justify="center">
+        <Grid item >
+          <Button
+            fullWidth
+            variant="contained"
+            color="secondary"
+            onClick = {resetGame}
+          >
+            Reset
+          </Button>
+        </Grid>
+      </Grid>
+      <>
+      <RoboDir/>  
+      </>
     </Grid>
     </Suspense>
   );
